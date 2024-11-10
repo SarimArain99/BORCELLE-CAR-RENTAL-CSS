dbff52c08a54f77a0b275ef6b3fc58984ab7b05e
Muhammad Sarim <sarimarain99@gmail.com>
2024-11-10 13:48:50 +0500

    updated

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,19 +1,19 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import "./global.css";
 
 export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <html>
-        <body>
-          <Header/>
-          {children}
-          <Footer/>
-          </body>
-      </html>
-    );
-  }
-  
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html>
+      <body>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
